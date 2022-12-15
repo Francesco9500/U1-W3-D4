8fa746346e136d4a6e491d6c2b1c6ff70ff0be7d
Francesco9500 <francescorg95@gmail.com>
2022-12-15 17:22:47 +0100

    D4

---
+++ index.js
@@ -0,0 +1,6 @@
+const addEventListener = () => {
+  const newTaskbtn = document.getElementById("new-task-btn");
+  newTaskbtn.addEventListener("click", () => {
+    console.log("nuovo task");
+  });
+};
